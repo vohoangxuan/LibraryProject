cb25904d6a4fa584e8ff11a2bbb15cb61981bfe5
vohoangxuan <hoangxuanvot@gmail.com>
2023-12-06 22:38:25 -0600

    Update AddMemberForm.java
    giao dien add member

--- src/guid/AddMemberForm.java
+++ src/guid/AddMemberForm.java
@@ -1,14 +1,18 @@
 package guid;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
@@ -26,211 +30,281 @@ public class AddMemberForm extends JFrame implements LibWindow{
 	
 	private boolean isInitialized = false;
 	private JPanel mainPanel;
-	private JLabel label1;
-    private JPanel panel1;
-    private JPanel panel4;
-    private JLabel label4;
+	private JPanel upperHalf;
+	private JPanel middleHalf;
+	private JPanel lowerHalf;
+	private JPanel topPanel;
+	private JPanel middlePanel;
+	private JPanel leftTextPanel;
+	private JPanel rightTextPanel;	
+	
+	
     private JTextField memberID;
-    private JPanel panel3;
-    private JLabel label3;
     private JTextField firstName;
-    private JPanel panel2;
-    private JLabel label2;
     private JTextField lastName;
-    private JPanel panel6;
-    private JLabel label6;
     private JTextField phoneNumber;
-    private JPanel panel5;
-    private JLabel label5;
     private JTextField state;
-    private JPanel panel7;
-    private JLabel label7;
     private JTextField city;
-    private JPanel panel8;
-    private JLabel label8;
     private JTextField zip;
-    private JPanel panel9;
-    private JLabel label9;
     private JTextField street;
     private JButton addNewMember;
     private JPanel lowerPanel;
-    private JPanel panelR1;
-    private JPanel panelR2;
     
 	public void init() {
 		mainPanel = new JPanel();
-        label1 = new JLabel();
-        panel1 = new JPanel();
-        panel4 = new JPanel();
-        label4 = new JLabel();
-        memberID = new JTextField();
-        panel3 = new JPanel();
-        label3 = new JLabel();
-        firstName = new JTextField();
-        panel2 = new JPanel();
-        label2 = new JLabel();
-        lastName = new JTextField();
-        panel6 = new JPanel();
-        label6 = new JLabel();
-        phoneNumber = new JTextField();
-        panel5 = new JPanel();
-        label5 = new JLabel();
-        state = new JTextField();
-        panel7 = new JPanel();
-        label7 = new JLabel();
-        city = new JTextField();
-        panel8 = new JPanel();
-        label8 = new JLabel();
-        zip = new JTextField();
-        panel9 = new JPanel();
-        label9 = new JLabel();
-        street = new JTextField();
-        addNewMember = new JButton();
-        addAddMemberButtonListener(addNewMember);
-
-        //======== this ========
-        var contentPane = getContentPane();
-        contentPane.setLayout(new BorderLayout(8, 8));
-
-        //---- label1 ----
-        label1.setText("Add new member");
-        label1.setHorizontalAlignment(SwingConstants.CENTER);
-        contentPane.add(label1, BorderLayout.NORTH);
-
-        //======== panel1 ========
-        {
-            panel1.setLayout(new FlowLayout());
-
-            //======== panel4 ========
-            {
-                panel4.setLayout(new FlowLayout());
-
-                //---- label4 ----
-                label4.setText("Member ID");
-                panel4.add(label4);
-
-                //---- memberID3 ----
-                memberID.setColumns(10);
-                panel4.add(memberID);
-
-                //======== panel3 ========
-                {
-                    panel3.setLayout(new FlowLayout());
-
-                    //---- label3 ----
-                    label3.setText("First Name");
-                    panel3.add(label3);
-
-                    //---- firstName ----
-                    firstName.setColumns(10);
-                    panel3.add(firstName);
-                }
-                panel4.add(panel3);
-            }
-            panel1.add(panel4);
-
-            //======== panel2 ========
-            {
-                panel2.setLayout(new FlowLayout());
-
-                //---- label2 ----
-                label2.setText("Last Name");
-                panel2.add(label2);
-
-                //---- lastName ----
-                lastName.setColumns(10);
-                panel2.add(lastName);
-
-                //======== panel6 ========
-                {
-                    panel6.setLayout(new FlowLayout());
-
-                    //---- label6 ----
-                    label6.setText("Phone number");
-                    panel6.add(label6);
-
-                    //---- phoneNumber ----
-                    phoneNumber.setColumns(10);
-                    panel6.add(phoneNumber);
-                }
-                panel2.add(panel6);
-            }
-            panel1.add(panel2);
-
-            //======== panel5 ========
-            {
-                panel5.setLayout(new FlowLayout());
-
-                //---- label5 ----
-                label5.setText("State");
-                panel5.add(label5);
-
-                //---- state ----
-                state.setColumns(10);
-                panel5.add(state);
-            }
-            panel1.add(panel5);
-
-            //======== panel7 ========
-            {
-                panel7.setLayout(new FlowLayout());
-
-                //---- label7 ----
-                label7.setText("City");
-                panel7.add(label7);
-
-                //---- city ----
-                city.setColumns(10);
-                panel7.add(city);
-            }
-            panel1.add(panel7);
-
-            //======== panel8 ========
-            {
-                panel8.setLayout(new FlowLayout());
-
-                //---- label8 ----
-                label8.setText("Zip");
-                panel8.add(label8);
-
-                //---- zip ----
-                zip.setColumns(10);
-                panel8.add(zip);
-            }
-            panel1.add(panel8);
-
-            //======== panel9 ========
-            {
-                panel9.setLayout(new FlowLayout());
-
-                //---- label9 ----
-                label9.setText("street");
-                panel9.add(label9);
-
-                //---- street ----
-                street.setColumns(10);
-                panel9.add(street);
-            }
-            panel1.add(panel9);
-        }
-        contentPane.add(panel1, BorderLayout.NORTH);
-
-        //---- addNewMember ----
-        addNewMember.setText("Add new member");
-        contentPane.add(addNewMember, BorderLayout.CENTER);
-        defineLowerPanel();
-        contentPane.add(lowerPanel, BorderLayout.SOUTH);
-        pack();
-        setLocationRelativeTo(getOwner());
-	}
-
-	public void defineLowerPanel() {
 		
-		JButton backToMainButn = new JButton("<= Back to Main");
-		backToMainButn.addActionListener(new BackToMainListener());
+    	defineUpperHalf();
+    	defineMiddleHalf();
+    	defineLowerHalf();
+    	
+    	BorderLayout bl = new BorderLayout();
+    	bl.setVgap(30);
+    	mainPanel.setLayout(bl);
+
+    	mainPanel.add(upperHalf, BorderLayout.NORTH);
+    	mainPanel.add(middleHalf, BorderLayout.CENTER);
+    	mainPanel.add(lowerHalf, BorderLayout.SOUTH);
+    	getContentPane().add(mainPanel);
+    	isInitialized(true);
+    	pack();
+	}
+	
+    private void defineUpperHalf() {
+		
+		upperHalf = new JPanel();
+		upperHalf.setLayout(new BorderLayout());
+		defineTopPanel();
+		defineMiddlePanel();
+		defineLowerPanel();
+		upperHalf.add(topPanel, BorderLayout.NORTH);
+		upperHalf.add(middlePanel, BorderLayout.CENTER);
+		upperHalf.add(lowerPanel, BorderLayout.SOUTH);
+		
+	}
+	private void defineMiddleHalf() {
+		middleHalf = new JPanel();
+		middleHalf.setLayout(new BorderLayout());
+		JSeparator s = new JSeparator();
+		s.setOrientation(SwingConstants.HORIZONTAL);
+		//middleHalf.add(Box.createRigidArea(new Dimension(0,50)));
+		middleHalf.add(s, BorderLayout.SOUTH);
+		
+	}
+	private void defineLowerHalf() {
+
+		lowerHalf = new JPanel();
+		lowerHalf.setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		JButton backButton = new JButton("<= Back to Main");
+		backButton.addActionListener(new BackToMainListener());
+		lowerHalf.add(backButton);
+		
+	}
+	private void defineTopPanel() {
+		topPanel = new JPanel();
+		JPanel intPanel = new JPanel(new BorderLayout());
+		intPanel.add(Box.createRigidArea(new Dimension(0,20)), BorderLayout.NORTH);
+		JLabel loginLabel = new JLabel("Add member");
+		Util.adjustLabelFont(loginLabel, Color.BLUE.darker(), true);
+		intPanel.add(loginLabel, BorderLayout.CENTER);
+		topPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		topPanel.add(intPanel);
+		
+	}
+	
+	
+	
+	private void defineMiddlePanel() {
+		middlePanel=new JPanel();
+		middlePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		defineLeftTextPanel();
+		defineRightTextPanel();
+		middlePanel.add(leftTextPanel);
+		middlePanel.add(rightTextPanel);
+	}
+	
+	private void defineLowerPanel() {
+		
 		lowerPanel = new JPanel();
-		lowerPanel.setLayout(new FlowLayout(FlowLayout.LEFT));;
-		lowerPanel.add(backToMainButn);
+		addNewMember = new JButton("Add new member");
+		addAddMemberButtonListener(addNewMember);
+		lowerPanel.add(addNewMember);
+		
+	}
+	
+	private void defineLeftTextPanel() {
+		leftTextPanel = new JPanel();
+		leftTextPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		JPanel leftTextPanel1 = new JPanel();
+		JPanel leftTextPanel2 = new JPanel();
+		leftTextPanel1.setLayout(new FlowLayout(FlowLayout.LEFT));
+		leftTextPanel2.setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		JPanel leftTextPanel11 = new JPanel();
+		leftTextPanel11.setLayout(new BorderLayout());
+		JPanel leftTextPanel12 = new JPanel();
+		leftTextPanel12.setLayout(new BorderLayout());
+		
+		JPanel panelMemIDLbl = new JPanel();
+		JLabel label4 = new JLabel("Member ID");
+		label4.setSize(180, 30);
+		panelMemIDLbl.add(label4);
+
+        //---- memberID3 ----
+		memberID = new JTextField();
+        memberID.setColumns(10);
+        JPanel panelMemFld = new JPanel(); 
+        panelMemFld.add(memberID);
+        
+        JPanel panelStreetLbl = new JPanel();
+        panelStreetLbl.setLayout(new FlowLayout());
+
+        JLabel label5 = new JLabel("Street");
+        label5.setSize(180, 30);
+        panelStreetLbl.add(label5);
+
+            //---- state ----
+        JPanel panelStreetFld = new JPanel();
+        street = new JTextField();
+        street.setColumns(10);
+        panelStreetFld.add(street);
+        
+        leftTextPanel11.add(panelMemIDLbl, BorderLayout.NORTH);
+        leftTextPanel11.add(panelStreetLbl, BorderLayout.CENTER);
+
+		leftTextPanel12.add(panelMemFld, BorderLayout.NORTH);
+        leftTextPanel12.add(panelStreetFld, BorderLayout.CENTER);
+        
+        //----------------------
+		JPanel leftTextPanel21 = new JPanel();
+		leftTextPanel21.setLayout(new BorderLayout());
+		JPanel leftTextPanel22 = new JPanel();	
+		leftTextPanel22.setLayout(new BorderLayout());
+		
+		JPanel panelFnLbl = new JPanel();
+		JLabel label6 = new JLabel("First Name");
+		panelFnLbl.add(label6);
+
+        //---- memberID3 ----
+		firstName = new JTextField();
+        firstName.setColumns(10);
+        JPanel panelFnFld = new JPanel(); 
+        panelFnFld.add(firstName);
+        
+        JPanel panelCityLbl = new JPanel();
+        panelCityLbl.setLayout(new FlowLayout());
+
+        JLabel label7 = new JLabel("City");
+        panelCityLbl.add(label7);
+
+            //---- city ----
+        JPanel panelCityFld = new JPanel();
+        city = new JTextField();
+        city.setColumns(10);
+        panelCityFld.add(city);
+        
+        leftTextPanel21.add(panelFnLbl, BorderLayout.NORTH);
+        leftTextPanel21.add(panelCityLbl, BorderLayout.CENTER);
+
+		leftTextPanel22.add(panelFnFld, BorderLayout.NORTH);
+        leftTextPanel22.add(panelCityFld, BorderLayout.CENTER);
+        
+		
+		leftTextPanel1.add(leftTextPanel11);
+		leftTextPanel1.add(leftTextPanel12);
+		leftTextPanel2.add(leftTextPanel21);
+		leftTextPanel2.add(leftTextPanel22);
+		leftTextPanel.add(leftTextPanel1);
+		leftTextPanel.add(leftTextPanel2);
+		
+	}
+	private void defineRightTextPanel() {
+		rightTextPanel = new JPanel();
+		rightTextPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		JPanel rightTextPanel1 = new JPanel();
+		JPanel rightTextPanel2 = new JPanel();
+		rightTextPanel1.setLayout(new FlowLayout(FlowLayout.LEFT));
+		rightTextPanel2.setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		JPanel rightTextPanel11 = new JPanel();
+		rightTextPanel11.setLayout(new BorderLayout());
+		JPanel rightTextPanel12 = new JPanel();
+		rightTextPanel12.setLayout(new BorderLayout());
+		
+		JPanel panelLnLbl = new JPanel();
+		JLabel label4 = new JLabel("Last Name");
+		label4.setSize(180, 30);
+		panelLnLbl.add(label4);
+
+        //---- Last name ----
+		lastName = new JTextField();
+        lastName.setColumns(10);
+        JPanel panelMemFld = new JPanel(); 
+        panelMemFld.add(lastName);
+        
+        JPanel panelZipLbl = new JPanel();
+        panelZipLbl.setLayout(new FlowLayout());
+
+        JLabel label5 = new JLabel("Zip");
+        label5.setSize(180, 30);
+        panelZipLbl.add(label5);
+
+            //---- zip ----
+        JPanel panelZipFld = new JPanel();
+        zip = new JTextField();
+        zip.setColumns(10);
+        panelZipFld.add(zip);
+        
+        rightTextPanel11.add(panelLnLbl, BorderLayout.NORTH);
+        rightTextPanel11.add(panelZipLbl, BorderLayout.CENTER);
+
+		rightTextPanel12.add(panelMemFld, BorderLayout.NORTH);
+        rightTextPanel12.add(panelZipFld, BorderLayout.CENTER);
+        
+        //----------------------
+		JPanel rightTextPanel21 = new JPanel();
+		rightTextPanel21.setLayout(new BorderLayout());
+		JPanel rightTextPanel22 = new JPanel();	
+		rightTextPanel22.setLayout(new BorderLayout());
+		
+		JPanel panelTelLbl = new JPanel();
+		JLabel label6 = new JLabel("Phone number");
+		panelTelLbl.add(label6);
+
+        //---- memberID3 ----
+		phoneNumber = new JTextField();
+        phoneNumber.setColumns(10);
+        JPanel panelFnFld = new JPanel(); 
+        panelFnFld.add(phoneNumber);
+        
+        JPanel panelStateLbl = new JPanel();
+        panelStateLbl.setLayout(new FlowLayout());
+
+        JLabel label7 = new JLabel("State");
+        panelStateLbl.add(label7);
+
+            //---- state ----
+        JPanel panelStateFld = new JPanel();
+        state = new JTextField();
+        state.setColumns(10);
+        panelStateFld.add(state);
+        
+        rightTextPanel21.add(panelTelLbl, BorderLayout.NORTH);
+        rightTextPanel21.add(panelStateLbl, BorderLayout.CENTER);
+
+		rightTextPanel22.add(panelFnFld, BorderLayout.NORTH);
+        rightTextPanel22.add(panelStateFld, BorderLayout.CENTER);
+        
+		
+		rightTextPanel1.add(rightTextPanel11);
+		rightTextPanel1.add(rightTextPanel12);
+		rightTextPanel2.add(rightTextPanel21);
+		rightTextPanel2.add(rightTextPanel22);
+		rightTextPanel.add(rightTextPanel1);
+		rightTextPanel.add(rightTextPanel2);
+		
 	}
 	
 	private void addAddMemberButtonListener(JButton butn) {
